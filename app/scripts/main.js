1f036978384f94dae81c4e65273d94115e4441a2
mberges92 <mberges92@gmail.com>
2015-02-16 21:42:53 +0100

    Parte de informacion de contacto, validacion simple

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,26 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function() {
+    $("#formulario").validate({
+        debug: true,
+        rules: {
+            nombre: "required",
+            apellidos: "required",
+            telefono: {
+                required: true,
+                digits: true,
+                minlength: 9,
+                maxlength: 9
+            },
+            email: {
+                required: true,
+                email: true,
+                minlength: 4
+            },
+            email2: {
+            	required: true,
+            	equalTo: email
+            }
+
+
+        }
+    }); // FIN DE VALIDATE()
+}); // FIN DE READY()
