1d4c59051197b2401bf6755d55662e00da9a0b51
mberges92 <mberges92@gmail.com>
2015-02-18 00:10:20 +0100

    scripts y metodos de validacion, jquery para el form, archivos php

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -13,21 +13,27 @@ $(document).ready(function() {
             email: {
                 required: true,
                 email: true,
+                minlength: 4,
                 remote: "php/validar_email.php" // dejo la ruta al php ya puesta
             },
             email2: {
                 required: true,
                 equalTo: email
             },
+            nombreParticular: {
+                required: true
+            },
             nombreEmpresa: {
                 required: true
             },
             nif: {
                 required: true,
-                remote: "php/validar_nif.php"  // dejo la ruta ya puesta
+                nifES: true,
+                remote: "php/validar_nif.php" // dejo la ruta ya puesta
             },
             cif: {
-                required: true
+                required: true,
+                cifES: true
             },
             direccion: {
                 required: true
@@ -39,14 +45,15 @@ $(document).ready(function() {
                 minlength: 5
             },
             iban: {
-                required: true
+                required: true,
+                iban: true
             },
             usuario: {
                 required: true,
                 minlength: 4
             },
             contrasenia: {
-                required: true,
+                required: true
             },
             contrasenia2: {
                 required: true,
@@ -54,4 +61,153 @@ $(document).ready(function() {
             }
         } // FIN DE RULES
     }); // FIN DE VALIDATE()
-}); // FIN DE READY()
+
+    // SI ES PARTICULAR, RELLENAR CAMPO DE NOMBRE(EN APARTADO FACTURACION) CON DATOS DE CONTACTO
+    $("#apellidos").focusout(function() {
+        if ($("#d_particular").is(":checked")) {
+            var nombre = $("#nombre").val();
+            var apellidos = $("#apellidos").val();
+
+            $("#nombreParticular").val(nombre + " " + apellidos);
+        }
+    });
+
+
+    // RELLENAR EL CODIGO POSTAL CON 0 A LA IZQUIERDA
+    $("#cp").focusout(function() {
+        var campoCP = $("#cp").val();
+        var cont = 0;
+
+        while (campoCP.length < 5) {
+            campoCP = "0" + campoCP;
+            cont++;
+        }
+        $("#cp").val(campoCP);
+
+    }); // FIN DEL CP CON CEROS
+
+    // CAMPO DEL USUARIO SE RELLENA CON EL CAMPO DE EMAIL2
+    $("#email2").focusout(function() {
+        $("#usuario").val($("#email2").val());
+    });
+
+
+
+    // CAMBIO CON BOTON, PARTICULAR - EMPRESA
+    $("input:radio").click(function() {
+
+        if ($("#d_particular").is(":checked")) {
+
+            // PARTE DE CIF A NIF
+            $("#lblcif").html("NIF:");
+            $("#lblcif").attr({
+                "for": "nif",
+                "id": "lblnif"
+            });
+            $("#cif").attr({
+                "name": "nif",
+                "id": "nif"
+            });
+
+            // PARTE DE EMPRESA A NOMBRE
+            $("#lblnombreEmpresa").html("Nombre:");
+            $("#lblnombreEmpresa").attr({
+                "for": "nombreParticular",
+                "id": "lblnombreParticular"
+            });
+            $("#nombreEmpresa").attr({
+                "name": "nombreParticular",
+                "id": "nombreParticular"
+            });
+            $("#nombreParticular").val("");
+
+        } // FIN DEL IF
+        if ($("#d_empresa").is(":checked")) {
+
+            // PARTE DE NIF A CIF LABEL
+            $("#lblnif").html("CIF:");
+            $("#lblnif").attr({
+                "for": "cif",
+                "id": "lblcif"
+            });
+            $("#nif").attr({
+                "name": "cif",
+                "id": "cif"
+            });
+
+            // PARTE DE NOMBRE A EMPRESA
+            $("#lblnombreParticular").html("Empresa:");
+            $("#lblnombreParticular").attr({
+                "for": "nombreEmpresa",
+                "id": "lblnombreEmpresa"
+            });
+            $("#nombreParticular").attr({
+                "name": "nombreEmpresa",
+                "id": "nombreEmpresa"
+            });
+            $("#nombreEmpresa").val("");
+
+        } // FIN DEL IF
+    }); // FIN DEL EVENTO CLICK DE INPUT RADIO
+
+
+
+
+}); // FIN DEL DOCUMENT READY
+
+
+// ARRAY PARA CODIGO POSTAL Y PROVINCIA
+var prov = new Array();
+prov[0] = "NO HAY";
+prov[1] = "Alava";
+prov[2] = "Albacete";
+prov[3] = "Alicante";
+prov[4] = "Almeria";
+prov[5] = "Avila";
+prov[6] = "Badajoz";
+prov[7] = "Illes Balears";
+prov[8] = "Barcelona";
+prov[9] = "Burgos";
+prov[10] = "Caceres";
+prov[11] = "Cadiz";
+prov[12] = "Castellon";
+prov[13] = "Ciudad Real";
+prov[14] = "Cordoba";
+prov[15] = "A Coruña";
+prov[16] = "Cuenca";
+prov[17] = "Girona";
+prov[18] = "Granada";
+prov[19] = "Guadalajara";
+prov[20] = "Guipuzcoa";
+prov[21] = "Huelva";
+prov[22] = "Huesca";
+prov[23] = "Jaen";
+prov[24] = "Leon";
+prov[25] = "Lleida";
+prov[26] = "La Rioja";
+prov[27] = "Lugo";
+prov[28] = "Madrid";
+prov[29] = "Malaga";
+prov[30] = "Murcia";
+prov[31] = "Navarra";
+prov[32] = "Ourense";
+prov[33] = "Asturias";
+prov[34] = "Palencia";
+prov[35] = "Las Palmas";
+prov[36] = "Pontevedra";
+prov[37] = "Salamanca";
+prov[38] = "S.C. Tenerife";
+prov[39] = "Cantabria";
+prov[40] = "Segovia";
+prov[41] = "Sevilla";
+prov[42] = "Soria";
+prov[43] = "Tarragona";
+prov[44] = "Teruel";
+prov[45] = "Toledo";
+prov[46] = "Valencia";
+prov[47] = "Valladolid";
+prov[48] = "Vizcaya";
+prov[49] = "Zamora";
+prov[50] = "Zaragoza";
+prov[51] = "Ceuta";
+prov[52] = "Melilla";
