6ea9686440f96f785f40a73254bc62de8748031d
mberges92 <mberges92@gmail.com>
2015-02-17 17:15:49 +0100

    Terminado esqueleto y rules sencillas, añadido fichero traduccion a ESP y carpeta php

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -13,14 +13,45 @@ $(document).ready(function() {
             email: {
                 required: true,
                 email: true,
-                minlength: 4
+                remote: "php/validar_email.php" // dejo la ruta al php ya puesta
             },
             email2: {
-            	required: true,
-            	equalTo: email
+                required: true,
+                equalTo: email
+            },
+            nombreEmpresa: {
+                required: true
+            },
+            nif: {
+                required: true,
+                remote: "php/validar_nif.php"  // dejo la ruta ya puesta
+            },
+            cif: {
+                required: true
+            },
+            direccion: {
+                required: true
+            },
+            cp: {
+                required: true,
+                digits: true,
+                maxlength: 5,
+                minlength: 5
+            },
+            iban: {
+                required: true
+            },
+            usuario: {
+                required: true,
+                minlength: 4
+            },
+            contrasenia: {
+                required: true,
+            },
+            contrasenia2: {
+                required: true,
+                equalTo: contrasenia
             }
-
-
-        }
+        } // FIN DE RULES
     }); // FIN DE VALIDATE()
 }); // FIN DE READY()
